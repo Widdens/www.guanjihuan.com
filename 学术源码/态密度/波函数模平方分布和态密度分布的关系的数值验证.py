ff0ce41695ccac5231fc5203108ad3a5fdee939f
guanjihuan <guanjihuan@163.com>
2020-06-25 06:43:57 +0800

    update

--- 学术源码/态密度/波函数模平方分布和态密度分布的关系的数值验证.py
+++ 学术源码/态密度/波函数模平方分布和态密度分布的关系的数值验证.py
@@ -20,19 +20,20 @@ def main():
     h0 = hamiltonian()
     dim = h0.shape[0]
     n = 4  # 选取第n个能级
-    eigenvalue, eigenvector = np.linalg.eig(h0)
+    eigenvalue, eigenvector = np.linalg.eig(h0)  # 本征值、本征矢
     # print(h0)
-    print('哈密顿量的维度：', dim)  # 哈密顿量的维度
-    print('本征矢的维度：', eigenvector.shape)  # 本征矢的维度
-    print('能级（未排序）：', eigenvalue)  # 输出本征值。因为体系是受限的，所以是离散的能级
-    print('选取第', n, '个能级，为',  eigenvalue[n-1])  # 从1开始算，查看第n个能级是什么（这里本征值未排序）
-    print('第', n, '个能级对应的波函数：', eigenvector[:, n-1])  # 查看第n个能级对应的波函数
-    print('\n波函数模的平方：', np.square(np.abs(eigenvector[:, n-1])))   # 查看第n个能级对应的波函数模的平方
+    # print('哈密顿量的维度：', dim)  # 哈密顿量的维度
+    # print('本征矢的维度：', eigenvector.shape)  # 本征矢的维度
+    # print('能级（未排序）：', eigenvalue)  # 输出本征值。因为体系是受限的，所以是离散的能级
+    # print('选取第', n, '个能级，为',  eigenvalue[n-1])  # 从1开始算，查看第n个能级是什么（这里本征值未排序）
+    # print('第', n, '个能级对应的波函数：', eigenvector[:, n-1])  # 查看第n个能级对应的波函数
+    print('\n波函数模的平方：\n', np.square(np.abs(eigenvector[:, n-1])))   # 查看第n个能级对应的波函数模的平方
     green = np.linalg.inv((eigenvalue[n-1]+1e-15j)*np.eye(dim)-h0)  # 第n个能级对应的格林函数
-    total = np.trace(np.imag(green))  # 求该能级格林函数的迹，对应的是总态密度（忽略系数）
+    total = np.trace(np.imag(green))  # 求该能级格林函数的迹，对应的是总态密度（忽略符号和系数）
     print('归一化后的态密度分布：')
     for i in range(dim):
         print(np.imag(green)[i, i]/total)  # 第n个能级单位化后的态密度分布
+    print('观察以上两个分布的数值情况，可以发现两者完全相同。')
 
 
 if __name__ == '__main__':  # 如果是当前文件直接运行，执行main()函数中的内容；如果是import当前文件，则不执行。
