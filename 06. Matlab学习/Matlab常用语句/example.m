b190be35ebd0ff6ca1a3f4942b554ec0c3122a29
guanjihuan <guanjihuan@163.com>
2020-07-28 10:27:19 +0800

    Update example.m

--- 06. Matlab学习/Matlab常用语句/example.m
+++ 06. Matlab学习/Matlab常用语句/example.m
@@ -10,13 +10,13 @@ cc1=zeros(2,3)  %零矩阵用zeros()
 cc2=eye(3,3)  %单位矩阵
 
 %矩阵乘积
-matrix1=[3,3;3,3]  %矩阵的书写格式是这样的，里面分号代表矩阵换一行。下标是从1开始记。
+matrix1=[3,3;3,3]  %里面分号代表矩阵换一行。下标是从1开始记。
 matrix2=[2,0;0,2]
 matrix_product_1=matrix1*matrix2  % *是正常的矩阵乘积
 matrix_product_2=matrix1.*matrix2  % .*是矩阵每个元素对应相乘
 
 %循环
-for i0=1:0.5:2  %循环内容为for到end。a:b:c代表最小为a，最大为c，步长为b，区间为左闭右闭[a,c]
+for i0=1:0.5:2  %循环内容为for到end。a:b:c代表最小为a，最大为c，步长为b
     for_result=i0+1i  %i在matlab中代表虚数，所以起变量名最好不要用i。要输出内容，后面不加分号即可
 end
 
@@ -31,7 +31,7 @@ matrix=[2,3;5,7]
 %求本征矢和本征值
 [V,D]=eig(matrix)  %在matlab中，V的列向量是本征矢，注意是列。D的对角上是对应本征值。
 %求逆
-inv1=inv(matrix)  %求逆可以这样写
+inv1=inv(matrix)  %求逆
 inv2=matrix^-1    %求逆也可以这样写
 %画图
 plot([0:20],[10:-1:-10],'-o')  %更多画图技巧可参考官方文档或网上资料
